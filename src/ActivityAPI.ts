7ab20852c0c5d8e78b969fe4c28a2f5b8146cab2
Rhinonuts11 <161979906+Rhinonuts11@users.noreply.github.com>
2025-07-19 11:30:30 +0100

    Updated ActivityAPI.ts

--- src/ActivityAPI.ts
+++ src/ActivityAPI.ts
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/node';
 import fetch, { Response } from 'node-fetch';
 
-const BASE_URL = 'https://[redacted]/v1/maylog-activity';
+const BASE_URL = 'https://api.syntaxical.lol/v1/maylog-activity';
 
 // todo: USE discord-embedbuilder FOR DEV COMMANDS
 
